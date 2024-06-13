0c2aa7157368d46cdc0a7f580c121c93fcdbbb31
Syozhi <syozhi@163.com>
2024-06-13 13:05:27 +0800

    第一次提交

---
+++ res/custom.js
@@ -0,0 +1,12 @@
+var CUSTOM_FONTS = [
+    // 在这里添加自定义字体，可以选择系统字体或字体文件
+    // 系统字体：{ "name": "制作器字体名", "font": "系统的字体名称" },系统字体名称就是平时所看到的字体名
+    // 字体文件：{ "name": "制作器字体名", "file": "字体文件的路径" },字体文件放置在fonts/文件夹下
+    // 例子：（不包括反斜杠）
+    // { "name": "图拉真体", "font": "Trajan Pro" },
+    // { "name": "草书", "file": "custom/caoshu.ttf" },
+    { "name": "图拉真体", "file": "custom/TrajanPro-Regular.ttf" },
+    { "name": "Webdings", "font": "Webdings" },
+    { "name": "华文琥珀", "font": "STHupo" },
+    { "name": "雷曼2", "file": "custom/rayman.ttf" },
+];
---
+++ res/templates.js
@@ -0,0 +1,19 @@
+var TEMPLATES = [
+	{name: "经典模板", path: "classic/classic"},
+	{name: "经典模板（卤粉）", path: "classic/lufen"},
+	{name: "经典卡牌模板", path: "classic/cards"},
+	{name: "Q版模板", path: "chibi"},
+	{name: "国战模板", path: "guozhan"},
+	{name: "三国杀2020模板", path: "diy2020"},
+	{name: "官方模板（自定配色）", path: "classic/custom-colored"},
+	{name: "桌游志·☆SP模板", path: "star-sp"},
+	{name: "桌游志·全幅模板", path: "spcloud"},
+	{name: "欢乐三国杀模板", path: "xhlsgs"},
+	{name: "移动版模板", path: "mobile"},
+	{name: "动漫模板", path: "anime"},
+	{name: "印花模板", path: "yinhua"},
+	{name: "YL超大幅模板", path: "ylhuge"},
+	{name: "帝国3模板", path: "aoe3"},
+	{name: "洛吧杀模板", path: "rockman"},
+	{name: "狼羊志模板", path: "langyangzhi"}
+];
